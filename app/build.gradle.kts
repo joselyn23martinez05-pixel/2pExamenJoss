66681fb2b869abf060a3b0aa97afb70627d61910
joselyn23martinez05 <joselyn23martinez05@gmail.com>
2026-05-10 01:20:36 -0600

    Commit 3 - Dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 }
 
 dependencies {
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.compose.foundation:foundation")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
