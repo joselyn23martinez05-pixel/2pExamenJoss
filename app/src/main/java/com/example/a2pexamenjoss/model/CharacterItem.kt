c2102f1f07edd82ea2912164f5127e402fecdaa4
joselyn23martinez05 <joselyn23martinez05@gmail.com>
2026-05-10 03:14:35 -0600

    Commit 4 - Creación de Clase e Imageness

--- app/src/main/java/com/example/a2pexamenjoss/model/CharacterItem.kt
+++ app/src/main/java/com/example/a2pexamenjoss/model/CharacterItem.kt
@@ -1,2 +1,10 @@
 package com.example.a2pexamenjoss.model
 
+data class CharacterItem(
+    val id: Int,
+    val name: String,
+    val image: Int,
+    val hoverImage: Int,
+    val background: Int,
+    val description: String
+)
