b4bfffdee00ce076b799285548922f45c3ea99a3
joselyn23martinez05 <joselyn23martinez05@gmail.com>
2026-05-10 13:21:50 -0600

    Commit 6 - Creación de Lista en Data

--- app/src/main/java/com/example/a2pexamenjoss/data/CharacterData.kt
+++ app/src/main/java/com/example/a2pexamenjoss/data/CharacterData.kt
@@ -1,2 +1,62 @@
 package com.example.a2pexamenjoss.data
 
+import com.example.a2pexamenjoss.R
+import com.example.a2pexamenjoss.model.CharacterItem
+
+val characterList = listOf(
+
+    CharacterItem(
+        id = 1,
+        name = "Parzival",
+        image = R.drawable.character1,
+        hoverImage = R.drawable.character1_alt,
+        background = R.drawable.bg1,
+        description = "El primer jugador en hallar la Llave de Cobre, Utiliza su vasto conocimiento de la cultura pop para descifrar los acertijos de Halliday y liberar a OASIS del control corporativo"
+
+    ),
+
+    CharacterItem(
+        id = 2,
+        name = "Artemis",
+        image = R.drawable.character2,
+        hoverImage = R.drawable.character2_alt,
+        background = R.drawable.bg2,
+        description = "Reconocida cazadora de tesoros y estratega de la resistencia, emplea su agilidad y puntería de élite para desmantelar operaciones enemigas y proteger el legado del sistema."
+    ),
+
+    CharacterItem(
+        id = 3,
+        name = "Anorak",
+        image = R.drawable.character3,
+        hoverImage = R.drawable.character3_alt,
+        background = R.drawable.bg3,
+        description = "Alto Mago y guardián del sistema; como entidad omnisciente de la simulación, posee el control sobre las leyes de la realidad virtual y custodia los secretos finales del gran premio."
+    ),
+
+    CharacterItem(
+        id = 4,
+        name = "Sho",
+        image = R.drawable.character4,
+        hoverImage = R.drawable.character4_alt,
+        background = R.drawable.bg4,
+        description = "Infiltrador especializado en sigilo, utiliza su rapidez extrema para flanquear las líneas enemigas y ejecutar ataques críticos sin ser detectado."
+    ),
+
+    CharacterItem(
+        id = 5,
+        name = "Daito",
+        image = R.drawable.character5,
+        hoverImage = R.drawable.character5_alt,
+        background = R.drawable.bg5,
+        description = "Guerrero de élite y maestro de armas, emplea su maestría con la katana y su capacidad de transformación para defender el honor del equipo en el campo de batalla."
+    ),
+
+    CharacterItem(
+        id = 6,
+        name = "Sorrento",
+        image = R.drawable.character6,
+        hoverImage = R.drawable.character6_alt,
+        background = R.drawable.bg6,
+        description = "Director de operaciones de la corporación IOI; utiliza recursos financieros ilimitados y tecnología militar de alta gama para eliminar a cualquier buscador independiente y asegurar el control total del sistema."
+    )
+)
